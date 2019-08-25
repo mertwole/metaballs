052a251d08610fddf143b6cbb852f99528c61399
mertwole <mertwole@gmail.com>
2019-08-25 20:29:13 +0300

    Metaballs3D_rayTracing project and remove comp_shader from Metaballs3D

---
+++ metaballs3D_rayTracing/ShaderProgram.cs
@@ -0,0 +1,122 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System.IO;
+
+namespace Metaballs3D
+{
+    public class ShaderProgram
+    {
+        int shader_program;
+
+        public ShaderProgram()
+        {
+            shader_program = GL.CreateProgram();
+        }
+
+        public int Compile()
+        {
+            GL.LinkProgram(shader_program);
+
+            string log = GL.GetProgramInfoLog(shader_program);
+            if (log != "")
+                throw new System.Exception(log);
+
+            return shader_program;
+        }
+
+        public ShaderProgram addFragmentShader(StreamReader source)
+        {
+            string code = source.ReadToEnd();
+            int shader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(shader, code);
+            GL.CompileShader(shader);
+
+            string log = GL.GetShaderInfoLog(shader);
+            if (log != "")
+                throw new System.Exception(log);
+
+            GL.AttachShader(shader_program, shader);
+
+            return this;
+        }
+
+        public ShaderProgram addVertexShader(StreamReader source)
+        {
+            string code = source.ReadToEnd();
+            int shader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(shader, code);
+            GL.CompileShader(shader);
+
+            string log = GL.GetShaderInfoLog(shader);
+            if (log != "")
+                throw new System.Exception(log);
+
+            GL.AttachShader(shader_program, shader);
+
+            return this;
+        }
+
+        public ShaderProgram addGeometryShader(StreamReader source)
+        {
+            string code = source.ReadToEnd();
+            int shader = GL.CreateShader(ShaderType.GeometryShader);
+            GL.ShaderSource(shader, code);
+            GL.CompileShader(shader);
+
+            string log = GL.GetShaderInfoLog(shader);
+            if (log != "")
+                throw new System.Exception(log);
+
+            GL.AttachShader(shader_program, shader);
+
+            return this;
+        }
+
+        public ShaderProgram addComputeShader(StreamReader source)
+        {
+            string code = source.ReadToEnd();
+            int shader = GL.CreateShader(ShaderType.ComputeShader);
+            GL.ShaderSource(shader, code);
+            GL.CompileShader(shader);
+
+            string log = GL.GetShaderInfoLog(shader);
+            if (log != "")
+                throw new System.Exception(log);
+
+            GL.AttachShader(shader_program, shader);
+
+            return this;
+        }
+
+        public ShaderProgram addTessControlShader(StreamReader source)
+        {
+            string code = source.ReadToEnd();
+            int shader = GL.CreateShader(ShaderType.TessControlShader);
+            GL.ShaderSource(shader, code);
+            GL.CompileShader(shader);
+
+            string log = GL.GetShaderInfoLog(shader);
+            if (log != "")
+                throw new System.Exception(log);
+
+            GL.AttachShader(shader_program, shader);
+
+            return this;
+        }
+
+        public ShaderProgram addTessEvaluationShader(StreamReader source)
+        {
+            string code = source.ReadToEnd();
+            int shader = GL.CreateShader(ShaderType.TessEvaluationShader);
+            GL.ShaderSource(shader, code);
+            GL.CompileShader(shader);
+
+            string log = GL.GetShaderInfoLog(shader);
+            if (log != "")
+                throw new System.Exception(log);
+
+            GL.AttachShader(shader_program, shader);
+
+            return this;
+        }
+    }
+}
